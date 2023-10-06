39bf8c7c828777cc2d15e23f13a3a879e0b82038
Islam walied <islam.walied96@gmail.com>
2023-10-06 16:37:55 +0300

    Z. Hard Compare

--- codeforces/assuit sheet/sheet#1/Z. Hard Compare.cpp
+++ codeforces/assuit sheet/sheet#1/Z. Hard Compare.cpp
@@ -1,26 +1,16 @@
 #include <iostream>
-#include <string>
+#include <cmath>
 
 using namespace std;
 
-int main() {
-    long long l1 ,l2,r1,r2,d,e;
-    cin >> l1  >> r1  >> l2 >> r2;
-    if ((l2 < l1 && r2 < l1) || (r1 < l2 && r1 < r2))
-    {
-        cout << -1;
+int main()
+{
+    long long x, y, z, w;
+    cin >> x >> y >> z >> w;
+    if (y * log(x) > w * log(z)) {
+        cout << "YES";
     }
     else {
-        if (l1 > l2)
-            d = l1;
-        else
-            d = l2;
-
-        if (r1 < r2)
-            e = r1;
-        else
-            e = r2;
-        cout << d << " " << e;
+        cout << "NO";
     }
-
 }
