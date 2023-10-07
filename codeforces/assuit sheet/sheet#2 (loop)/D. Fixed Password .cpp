f487de7f795d988da3936d97ddbae35d11178aee
Islam walied <islam.walied96@gmail.com>
2023-10-07 17:34:53 +0300

    D. Fixed Password

---
+++ codeforces/assuit sheet/sheet#2 (loop)/D. Fixed Password .cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long long a;
+    for (int i = 0; i < a; ++i) {
+        cin >> a;
+        if(a == 1999) {
+            cout << "Correct\n";
+            break;
+        }
+        else
+            cout << "Wrong\n";
+    }
+}
